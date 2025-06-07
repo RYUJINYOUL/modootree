b60c0fc1b3f90dd7fea2fe8aed72d2bef92da422
youssi <zzz123zz12@naver.com>
2025-06-07 23:23:59 +0900

    wieiigue

--- components/edit/EditorCanvas.jsx
+++ components/edit/EditorCanvas.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { useEffect, useState } from 'react';
 import { ComponentLibrary } from '@/components/edit/ComponentLibrary';
 
 export default function EditorCanvas({ components, setComponents }) {
