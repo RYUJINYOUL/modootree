81deefaed7437cac66fda6f20a20881813f63738
youssi <zzz123zz12@naver.com>
2025-06-18 18:58:46 +0900

    third

--- firebase.js
+++ firebase.js
@@ -1,19 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCScPIXaFDAE1yIg_cxEQq0rjXKipCnOIw",
-  authDomain: "test-efe1e.firebaseapp.com",
-  projectId: "test-efe1e",
-  storageBucket: "test-efe1e.firebasestorage.app",
-  messagingSenderId: "845155971321",
-  appId: "1:845155971321:web:6f207979920222db8c7228"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
-// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 
@@ -21,5 +18,4 @@ export const storage = getStorage(app);
 export const db = getFirestore(app);
 // export const auth = getAuth(app);
 
-export default app
-// export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+export default app;
