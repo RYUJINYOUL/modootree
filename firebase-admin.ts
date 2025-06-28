44e8a50ae310e66d5b5260cc62731feba331a0d8
junyull <dalkomme@gmail.com>
2025-06-28 22:18:53 +0900

    djjskwkwfdfhdjafkjd

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -2,10 +2,9 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    const projectId = process.env.FIREBASE_PROJECT_ID;
+    const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
     const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY;
-
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
     if (!projectId || !clientEmail || !privateKey) {
       throw new Error('Missing Firebase Admin credentials');
@@ -15,7 +14,8 @@ if (!admin.apps.length) {
       credential: admin.credential.cert({
         projectId,
         clientEmail,
-        privateKey: privateKey.replace(/\\n/g, '\n'),
+        // privateKey에서 따옴표 제거
+        privateKey: privateKey.replace(/"/g, ''),
       }),
     });
   } catch (error) {
