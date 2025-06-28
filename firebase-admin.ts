66a8647bb6c6d10d26044a653fc83116f28635eb
junyull <dalkomme@gmail.com>
2025-06-28 22:24:17 +0900

    djjskwkwfdjkfdkjshkjd

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -4,18 +4,22 @@ if (!admin.apps.length) {
   try {
     const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
     const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+    let privateKey = process.env.FIREBASE_PRIVATE_KEY;
 
     if (!projectId || !clientEmail || !privateKey) {
       throw new Error('Missing Firebase Admin credentials');
     }
 
+    // private key 처리
+    if (privateKey.includes('\\n')) {
+      privateKey = privateKey.replace(/\\n/g, '\n');
+    }
+    
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId,
         clientEmail,
-        // privateKey에서 따옴표 제거
-        privateKey: privateKey.replace(/"/g, ''),
+        privateKey,
       }),
     });
   } catch (error) {
