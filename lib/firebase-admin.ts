13ab3d2c36801c8897c64d84a0464f7b5fdf288c
junyull <dalkomme@gmail.com>
2025-10-02 00:36:22 +0900

    modootreeeee

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -5,9 +5,9 @@ import { getAuth } from 'firebase-admin/auth';
 // Firebase Admin SDK 초기화
 const firebaseAdminConfig = {
   credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY_BASE64,
   }),
 };
 
